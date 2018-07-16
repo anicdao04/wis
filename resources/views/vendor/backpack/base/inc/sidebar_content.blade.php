7bcf208162bc3e4e55baf7101370059cc0a11c0b
Michael f. Munoz <michaelfmunoz@Michael-f-Munozs-MacBook-Pro.local>
2018-07-16 10:18:43 +0800

    Updated sidebar

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,15 +1,15 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
-<li class="treeview">
+{{-- <li class="treeview">
   <a href="#"><i class="fa fa-key"></i> <span>Enrollment Module</span> <i class="fa fa-angle-left pull-right"></i></a>
   <ul class="treeview-menu">
     <li><a href="{{ url('admin/student') }}"><i class="fa fa-tag"></i> <span>Students</span></a></li>
     <li><a href="{{ url('admin/misc') }}"><i class="fa fa-tag"></i> <span>Miscellaneous</span></a></li>
     <li><a href="{{ url('admin/level') }}"><i class="fa fa-tag"></i> <span>Levels</span></a></li>
   </ul>
-</li>
+</li> --}}
 
-<li class="treeview">
+{{-- <li class="treeview">
   <a href="#"><i class="fa fa-tasks"></i> <span>System Management</span> <i class="fa fa-angle-left pull-right"></i></a>
   	<ul class="treeview-menu">
 	    <li>
@@ -28,7 +28,7 @@
 			<a href="{{ url(config('backpack.base.route_prefix', 'admin') . '/section_management') }}"><span>Section Management</span></a>
 	    </li>
   	</ul>
-</li>
+</li> --}}
 
 <li><a href="{{ url(config('backpack.base.route_prefix').'/page') }}"><i class="fa fa-file-o"></i> <span>Pages</span></a></li>
 
